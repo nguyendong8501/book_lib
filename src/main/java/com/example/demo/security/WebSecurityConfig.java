b9efffc630a73eeb5aab446acad1c9a0a5231748
nguyenthedong <nguyenthedong8501@gmail.com>
2023-04-18 17:58:41 +0700

    update

--- src/main/java/com/example/demo/security/WebSecurityConfig.java
+++ src/main/java/com/example/demo/security/WebSecurityConfig.java
@@ -62,9 +62,9 @@ public class WebSecurityConfig {
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http.cors().and().csrf().disable().exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
-				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().authorizeHttpRequests()
-				.requestMatchers("/api/auth/**").permitAll().requestMatchers("/api/test/**").permitAll()
-				.requestMatchers("/api/user/**").permitAll().anyRequest().authenticated();
+				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+				.authorizeHttpRequests().requestMatchers("/api/auth/**").permitAll().requestMatchers("/api/test/**")
+				.permitAll().anyRequest().authenticated();
 
 		http.authenticationProvider(authenticationProvider());
 
